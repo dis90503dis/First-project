df89d5f2fea3dc3792b66cff573db43ed675ddc8
dis90503dis <dis90503dis@gmail.com>
2023-11-28 17:08:01 +0800

    cart change

--- js/cart.js
+++ js/cart.js
@@ -1,16 +1,27 @@
-function displaySelectedValue() {
-    var selectElement = document.getElementById('numericSelect');
-    var selectedValue = selectElement.value;
-    var displayElement = document.getElementById('selectedValue');
-    document.getElementById("selectedValue").innerHTML = selectedValue + '幣/局';
-}
 var counterInput = document.getElementById('counter');
-    function increment() {
-        counterInput.value = parseInt(counterInput.value, 10) + 1;
-    }
-    function decrement() {
-        var currentValue = parseInt(counterInput.value, 10);
-        if (currentValue > 0) {
-        counterInput.value = currentValue - 1;
+        function increment() {
+            counterInput.value = parseInt(counterInput.value, 10) + 1;
+            updateTotal();
         }
-    }
+        function decrement() {
+            var currentValue = parseInt(counterInput.value, 10);
+            if (currentValue > 1) {
+                counterInput.value = currentValue - 1;
+                updateTotal();
+            }
+        }
+        function displaySelectedValue() {
+            var selectElement = document.getElementById('numericSelect');
+            var selectedValue = parseInt(selectElement.value, 10);
+            var displayElement = document.getElementById('selectedValue');
+            displayElement.innerHTML = selectedValue + '幣/局';
+            updateTotal();
+        }
+        function updateTotal() {
+            var selectElement = document.getElementById('numericSelect');
+            var selectedValue = parseInt(selectElement.value, 10);
+            var quantity = parseInt(counterInput.value, 10);
+            var totalValue = selectedValue * quantity;
+            var totalElement = document.getElementById('totalValue');
+            totalElement.innerHTML = '總價: ' + totalValue + ' 幣';
+        }
