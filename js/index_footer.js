b2274ef4cf70eb9ad14f28a747f349f499751bce
dis90503dis <dis90503dis@gmail.com>
2023-12-12 16:30:32 +0800

    1

--- js/index_footer.js
+++ js/index_footer.js
@@ -1,8 +1,12 @@
+var footerBgc = document.getElementById('footerBgc'); 
+var footerSearchbox = document.getElementById('footerSearchbox');
 function toggleSearch() {
     var searchBox = document.getElementById('searchBox');
     searchBox.style.display = 'block';
+    footerBgc.style.display = 'none';
 }
 document.addEventListener('touchmove', function(event){
 var searchBox = document.getElementById('searchBox');
 searchBox.style.display = 'none';
-})
+footerBgc.style.display = 'block';
+});
