057b5dc45613de594bf05802c1db7f796e3a2fe5
dis90503dis <dis90503dis@gmail.com>
2023-12-14 19:36:12 +0800

    1

--- js/index_footer.js
+++ js/index_footer.js
@@ -4,6 +4,7 @@ function toggleSearch() {
     var searchBox = document.getElementById('searchBox');
     searchBox.style.display = 'block';
     footerBgc.style.display = 'none';
+    searchBox.focus();
 }
 document.addEventListener('touchmove', function(event){
 var searchBox = document.getElementById('searchBox');
