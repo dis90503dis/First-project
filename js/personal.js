602e13bffb5e4f745281b0d3b51411d4e3b3b636
dis90503dis <dis90503dis@gmail.com>
2023-12-14 21:14:07 +0800

    1

--- js/personal.js
+++ js/personal.js
@@ -1,33 +1,60 @@
-var windowVW = $(window).width()
-if(windowVW>768){
-    function changeImage(newImageUrl) {
-        var largeImage = document.getElementById('largeImage');
-        largeImage.src = newImageUrl;
-    }
-    let imgWidth = $('#thumbnails img').width()
-    let imgCount = $('#thumbnails img').length
-    $('#thumbnails img:first').addClass('clicked')
-    let index = 0
+$(document).ready(function() {
+    var windowVW = $(window).width();
+    var imgWidth = $('#thumbnails img').width();
+    var imgCount = $('#thumbnails img').length;
 
-    $('#thumbnails img').click(function(){
-        index = $(this).index()
-        $('#thumbnails').animate({
-            left: imgWidth * index * -1 -5 * index,
-        })
-        $(this).addClass('clicked')
-        $('#thumbnails img').not(this).removeClass('clicked')
-    })
-    $('#leftIcon').click(function(){
-        $('#thumbnails').animate({
-            left: 0,
-        })
-    })
-    $('#rightIcon').click(function(){
-        $('#thumbnails').animate({
-            left: -1*(imgCount-1)*(imgWidth+5),
-        })
-    })
-}
-else{
-    
-}
+    if (windowVW > 768) {
+        $('#thumbnails img').click(function() {
+            var newImageUrl = $(this).attr('src');
+            $('#largeImage').attr('src', newImageUrl);
+        });
+        $('#thumbnails img:first').addClass('clicked');
+        
+        var index = 0;
+
+        $('#thumbnails img').click(function() {
+            index = $(this).index();
+            $('#thumbnails').animate({
+                left: imgWidth * index * -1 - 5 * index,
+            });
+            $(this).addClass('clicked').siblings().removeClass('clicked');
+        });
+
+        $('#leftIcon').click(function() {
+            index = (index - 1 + imgCount) % imgCount;
+            var newImageUrl = $('#thumbnails img').eq(index).attr('src');
+            $('#thumbnails').animate({
+                left: imgWidth * index * -1 - 5 * index,
+            });
+            $('#largeImage').attr('src', newImageUrl);
+            $('#thumbnails img').eq(index).addClass('clicked').siblings().removeClass('clicked');
+        });
+        $('#rightIcon').click(function() {
+            index = (index + 1) % imgCount;
+            var newImageUrl = $('#thumbnails img').eq(index).attr('src');
+            $('#thumbnails').animate({
+                left: imgWidth * index * -1 - 5 * index,
+            });
+            $('#largeImage').attr('src', newImageUrl);
+            $('#thumbnails img').eq(index).addClass('clicked').siblings().removeClass('clicked');
+        });
+    } else {
+        $('#thumbnails img').click(function() {
+            var newImageUrl = $(this).attr('src');
+            $('#lightboxImage').attr('src', newImageUrl);
+            $('#liGhtbox').addClass('show'); // Assuming you have a class for showing the lightbox
+        });
+
+        $('#leftIcon').click(function() {
+            $('#thumbnails').animate({
+                left: imgWidth,
+            });
+        });
+
+        $('#rightIcon').click(function() {
+            $('#thumbnails').animate({
+                left: -1 * imgWidth,
+            });
+        });
+    }
+});
