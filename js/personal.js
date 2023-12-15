c8e6297e84b37afb1c6e0eb7693257f2ccec218a
dis90503dis <dis90503dis@gmail.com>
2023-12-15 23:45:42 +0800

    1

--- js/personal.js
+++ js/personal.js
@@ -2,23 +2,20 @@ $(document).ready(function() {
     var windowVW = $(window).width();
     var imgWidth = $('#thumbnails img').width();
     var imgCount = $('#thumbnails img').length;
-
+    var index = 0;
     if (windowVW > 768) {
         $('#thumbnails img').click(function() {
             var newImageUrl = $(this).attr('src');
             $('#largeImage').attr('src', newImageUrl);
         });
         $('#thumbnails img:first').addClass('clicked');
-        var index = 0;
-
         $('#thumbnails img').click(function() {
             index = $(this).index();
             $('#thumbnails').animate({
                 left: imgWidth * index * -1 - 5 * index,
             });
             $(this).addClass('clicked').siblings().removeClass('clicked');
-        });
-
+        })
         $('#leftIcon').click(function() {
             $('#thumbnails').animate({
                 left: 0,
@@ -30,18 +27,19 @@ $(document).ready(function() {
             });
         });
     }else {
-    
+        var i = 0;
         $('#thumbnails img').click(function() {
         });
-
         $('#leftIcon').click(function() {
+            i = i + 1
             $('#thumbnails').animate({
-                left: imgWidth,
+                left:   i * imgWidth,
             });
         });
         $('#rightIcon').click(function() {
+            i = i - 1
             $('#thumbnails').animate({
-                left: -1 * imgWidth,
+                left: i * imgWidth,
             });
         });
     
